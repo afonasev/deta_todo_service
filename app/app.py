7e74dd67db66c372f12421c86914ad4ecca05535
Afonasev Evgeniy <ea.afonasev@gmail.com>
2022-06-15 12:17:34 +0500

    feat(settings): add cors settings

--- app/app.py
+++ app/app.py
@@ -2,6 +2,7 @@ import logging
 
 import sentry_sdk
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from app.ports.api.users.resources import router as users_router
 from app.settings import settings
@@ -13,18 +14,31 @@ def create_app() -> FastAPI:
         version='1.0',
     )
 
-    _init_logging()
-    _init_sentry()
+    _configure_logging()
+    _configure_sentry()
+    _configure_cors(app)
 
     app.include_router(users_router)
 
     return app
 
 
-def _init_logging() -> None:
+def _configure_logging() -> None:
     logging.basicConfig(format=settings.LOGGING_FORMAT, level=settings.LOGGING_LEVEL)
 
 
-def _init_sentry() -> None:
+def _configure_sentry() -> None:
     sentry_sdk.init()
     # todo: install middleware
+
+
+def _configure_cors(app: FastAPI) -> None:
+    # Set all CORS enabled origins
+    if settings.BACKEND_CORS_ORIGINS:
+        app.add_middleware(
+            CORSMiddleware,
+            allow_origins=settings.BACKEND_CORS_ORIGINS,
+            allow_credentials=True,
+            allow_methods=['*'],
+            allow_headers=['*'],
+        )
--- app/settings.py
+++ app/settings.py
@@ -1,16 +1,29 @@
-from pydantic import BaseSettings, validator
+# noqa:N805
+from pydantic import AnyHttpUrl, BaseSettings, validator
 
 
 class Settings(BaseSettings):
+    # Common settings
     PROJECT_NAME: str = 'Deta Todo Service'
-
+    BACKEND_CORS_ORIGINS: list[AnyHttpUrl] = []
     # WPS323 - Found `%` string formatting
     LOGGING_FORMAT: str = (
         '%(asctime)s %(levelname)s %(name)s %(message)s'  # noqa:WPS323
     )
     LOGGING_LEVEL: str = 'INFO'
 
+    # Application specific settings
     DETA_PROJECT_KEY: str
+
+    @validator('BACKEND_CORS_ORIGINS', pre=True)
+    def assemble_cors_origins(
+        cls, addresses: str | list[str]  # noqa:N805
+    ) -> list[str] | str:
+        if not isinstance(addresses, str):
+            return addresses
+        if not addresses.startswith('['):
+            return [address.strip() for address in addresses.split(',')]
+        return addresses
 
     @validator('*', pre=True)
     def empty_str_to_none(cls, input_value):  # noqa:N805
